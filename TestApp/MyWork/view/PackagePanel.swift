e0b558de77b7cd69031c7efb211670d3d22bee90
liuxu <chuyang009@163.com>
2024-07-28 02:18:41 +0800

    aa

---
+++ TestApp/MyWork/view/PackagePanel.swift
@@ -0,0 +1,20 @@
+//
+//  PackagePanel.swift
+//  TestApp
+//
+//  Created by 刘东旭 on 2024/7/28.
+//
+
+import UIKit
+
+class PackagePanel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
